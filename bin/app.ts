1782d051b8465f57093e0bec7cfebd26a18aff81
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-08-31 14:45:59 +0700

    Activate cluster

--- bin/app.ts
+++ bin/app.ts
@@ -1,10 +1,16 @@
+import cluster from 'node:cluster'
+import process from 'node:process'
+
 import { App } from '../src/core'
 import { AccountRepository, AwardRepository } from '../src/repositories'
 import * as mongo from '../src/mongodb'
+import logger from '../src/utils/logger'
+
+const numCPUs = 2 // or can also use `availableParallelism` from `node:os` library
 
 let app: App
 
-const start = async () => {
+const start = async (pid: number) => {
   await mongo.connect()
 
   const accountRepository = new AccountRepository()
@@ -12,7 +18,17 @@ const start = async () => {
 
   app = new App(accountRepository, awardRepository)
 
-  app.startServer()
+  app.startServer(pid)
 }
 
-start()
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker) => {
+    logger.info(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  start(process.pid)
+}
